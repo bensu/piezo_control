451a36ba1e3556e6d3209d0b31ef32250c972db2
Sebastian Bensusan <sbensu@gmail.com>
2014-04-03 11:47:01 -0300

    Works OK, Z meassures differently

--- accelerometer/accelerometer.ino
+++ accelerometer/accelerometer.ino
@@ -1,25 +1,4 @@
-/**
- * Written by Christopher Meyer, July 2011
- * Version 1.0
- * 
- * Simple example for MMA7361 library.
- * 
- * Program will output a comma delimited reading of all three accelerometer
- * axis on the MMA7361.
- * 
- */
-
 #include "Accelerometer.h"
-
-/**
- * Pin defines. Library was developed on a Modern Device MMA7361 Module. The
- * G and Vin pins are attached to A1 and A2 respectively. Those pins output
- * a low and a high respectively. The 3 axes are attached to the next 3 pins,
- * A3, A4, A5.
- * 
- */
-// #define GND_PIN A0
-// #define VCC_PIN A5
 
 #define X_PIN A0
 #define Y_PIN A1
@@ -31,9 +10,8 @@
 #define FILTER 50000
 
 long int counter;
-int i;
 
-Accelerometer acc = Accelerometer(X_PIN, Y_PIN, Z_PIN);
+Acc acc = Acc(X_PIN, Y_PIN, Z_PIN);
 
 void setup(void) {
     delay(250); //Allow the chip to stop shaking from the reset press
@@ -41,7 +19,6 @@ void setup(void) {
     Serial.begin(9600);
     Serial.println("Start");
     counter = 0;
-    i = 0;
 }
 
 /**
@@ -50,26 +27,16 @@ void setup(void) {
  */
 void loop(void) {
     char otherChar;
-    short int i;
-    i = 0;
     if (++counter > FILTER) {
         counter = 0;
-        int value;
-        
-        switch (i) {
-            case 0:
-              value = analogRead(A0);
-              break;
-            case 1:
-              value = analogRead(A1);
-              break;
-            case 2:
-              value = analogRead(A2);
-              break;
+        double sum = 0;
+        for(int i=0; i<3; i++) {
+            double value = Acc::to_v(acc.take_sample(AVERAGING_POINTS,i));
+            otherChar = (i==2) ? '\n' : ',';
+            Serial.print(value);
+            Serial.print(otherChar);
+            sum = sum + sq(value);
         }
-        otherChar = (i==2) ? '\n' : ',';
-        // i = (i + 1) % 3;
-        Serial.print(value);
-        Serial.print(otherChar);
+        // Serial.println(sqrt(sum)); 
     }
 }
